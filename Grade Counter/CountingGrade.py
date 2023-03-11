31cf8b29bd348f142e092cb509010411fbeee955
Ethicalsahuji <mesh17563@gmail.com>
2023-03-11 21:36:56 +0530

    The Counting Grade simple Program

---
+++ Grade Counter/CountingGrade.py
@@ -0,0 +1,15 @@
+print("Chalo Tumhara Grade Pta Kare !!")
+
+a = int(input("Enter Your Marks:--"))
+if (a>100):
+    print("Abe Saale Sahi Number Dal (0-100) Ke Bich Ka.🤬")
+elif a>=80:
+        print("You Have 'A' Grade.. Excellent !Keep it Up! 😍")
+elif a>=60:
+    print("Your Grade is 'B'. Very Good! 🤞")
+elif a>=40:
+    print("You Have Grade 'c'. Best of Luck Next Time 🤷‍♂️")
+elif a>=0:
+    print("You need to improve your Performance.. Very BAD..D Grade you Deserves😒")
+else:
+    print("Abe Saale Sahi Number Dal (0-100) Ke Bich Ka.🤬")
